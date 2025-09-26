4e1880cdfe645f27090625bb397edd3358d2d2cc
Denisu <brokenhandq3@yandex.ru>
2025-09-26 09:51:37 +0300

    streamin with loading 5

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,15 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-// import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import {
+   fetchRevenue, 
+  // fetchLatestInvoices, 
+  // fetchCardData 
+} from '../lib/data';
 import { Suspense } from 'react';
  
 export default async function Page() {
-  // const revenue = await fetchRevenue()
+  const revenue = await fetchRevenue()
   // const latestInvoices = await fetchLatestInvoices()
   // const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await  fetchCardData()
 
@@ -31,7 +35,7 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
+        <RevenueChart revenue={revenue}  />
         {/* <Suspense fallback={<div>LatestInvoices</div>}> */}
         {/* <LatestInvoices latestInvoices={latestInvoices}> */}
         {/* </LatestInvoices> */}
